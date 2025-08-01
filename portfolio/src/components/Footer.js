d2341c91a4f5f92e4f0aa8cbba4dc80e04dc1287
ninankhwashu <ninankhwashu@gmail.com>
2025-08-01 17:33:41 +0200

    Remove footer contact icons

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,61 +1,13 @@
-import React, { useEffect } from "react";
-import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
-import Tooltip from "bootstrap/js/dist/tooltip";
-import "bootstrap/dist/js/bootstrap.bundle.min";
+import React from "react";
 import "../styles/Footer.css";
 
 function Footer() {
-  useEffect(() => {
-    const tooltipTriggerList = document.querySelectorAll(
-      '[data-bs-toggle="tooltip"]'
-    );
-    tooltipTriggerList.forEach((tooltipTriggerEl) => {
-      new Tooltip(tooltipTriggerEl);
-    });
-  }, []);
-
   return (
     <footer className="footer text-white">
-      <div className="container">
-        <div className="row align-items-center text-center text-md-start">
-          <div className="col-md-6 mb-3 mb-md-0">
-            <p className="mb-0">
-              © {new Date().getFullYear()} Nina Nkhwashu. All Rights Reserved.
-            </p>
-          </div>
-          <div className="col-md-6">
-            <div className="social-icons d-flex justify-content-md-end justify-content-center gap-4">
-              <a
-                href="https://github.com/ninankhwashu"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-white"
-                data-bs-toggle="tooltip"
-                title="View my GitHub"
-              >
-                <FaGithub size={24} />
-              </a>
-              <a
-                href="https://www.linkedin.com/in/ninankhwashu/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-white"
-                data-bs-toggle="tooltip"
-                title="Connect on LinkedIn"
-              >
-                <FaLinkedin size={24} />
-              </a>
-              <a
-                href="mailto:ninankhwashu@gmail.com"
-                className="text-white"
-                data-bs-toggle="tooltip"
-                title="Send me an email"
-              >
-                <FaEnvelope size={24} />
-              </a>
-            </div>
-          </div>
-        </div>
+      <div className="container text-center">
+        <p className="mb-0">
+          © {new Date().getFullYear()} Nina Nkhwashu. All Rights Reserved.
+        </p>
       </div>
     </footer>
   );
