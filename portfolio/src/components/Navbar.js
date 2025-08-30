b029428658c744c0d27541dcdc2dde6a83223a72
gititbunny <ninankhwashu@gmail.com>
2025-08-30 21:44:28 +0200

    Update nav bar with skills

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -23,11 +23,11 @@ function CustomNavbar() {
         <Navbar.Toggle aria-controls="navbar-nav" />
         <Navbar.Collapse id="navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#projects">Projects</Nav.Link>
-            <Nav.Link href="#experience">Experience</Nav.Link>
             <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#skills">Skills</Nav.Link>
+            <Nav.Link href="#experience">Experience</Nav.Link>
             <Nav.Link href="#contact">Contact</Nav.Link>
-            <Nav.Link href="#blog">Blog</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
