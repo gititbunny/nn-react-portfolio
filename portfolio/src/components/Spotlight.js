29f96bbedceabca928b83a847ca2112dfbb7f372
gititbunny <ninankhwashu@gmail.com>
2025-09-27 13:54:59 +0200

    Update spotlight info

--- portfolio/src/components/Spotlight.js
+++ portfolio/src/components/Spotlight.js
@@ -8,14 +8,14 @@ function Spotlight() {
         <div class="profile-image mb-4 mb-md-0">
           <img
             src="/media/pp.jpg"
-            alt="Portrait of Nina Nkhwashu, Junior Full-Stack Developer"
+            alt="Portrait of Nina Nkhwashu, Full-Stack Developer"
           />
         </div>
 
         <div class="spotlight-text px-4">
           <h1 id="spotlight-heading">Hi there 👽, I’m Nina!</h1>
           <p class="tagline">
-            A Junior Full-Stack Developer with a creative edge. Skilled in
+            A Full-Stack Developer with a creative edge. Skilled in
             React, Python, JavaScript, and APIs.{" "}
             <em>I turn ideas into experiences that make an impact.</em>
           </p>
