0dfe89e37a7d3b6858a039ea4e65aae595853221
gititbunny <ninankhwashu@gmail.com>
2025-09-27 14:31:52 +0200

    Update stats counter

--- portfolio/src/components/Skills.js
+++ portfolio/src/components/Skills.js
@@ -73,7 +73,7 @@ function Skills() {
 
         <div className="stats-row">
           <StatCounter label="Projects Completed" end={32} />
-          <StatCounter label="GitHub Contributions" end={790} />
+          <StatCounter label="GitHub Contributions" end={800} />
           <StatCounter label="Tools & Technologies" end={34} />
         </div>
       </div>
